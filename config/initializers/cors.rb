2cb580d0fa8b4d6d34bad7d2eb06247d773358cf
Alex <alex@planomatic.com>
2025-02-28 11:02:01 -0700

    406 error debugging

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,15 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins '*' # Change this to the specific origin(s) you want to allow
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
+# end
+
+config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # Change this to the specific origin(s) you want to allow
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options], expose: ['Authorization']
   end
 end
