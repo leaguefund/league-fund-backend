c6f5f242a7716e1fabcd3b47400b202c1d2d4833
Alex <alex@planomatic.com>
2025-02-28 11:04:28 -0700

    406 error debugging 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 #   end
 # end
 
-config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options], expose: ['Authorization']
