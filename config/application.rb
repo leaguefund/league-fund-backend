7806401451bb194d5d4b394ad5d7b3468b20b23f
Alex <alex@planomatic.com>
2025-02-28 11:17:39 -0700

    406 error debugging 5

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,15 @@ module LeagueFundBackend
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*' # Update this to restrict origins as needed
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
