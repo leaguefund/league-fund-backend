8b84c6bbc4961d9ababeeaad2d522a6c12c3e431
Alex <alex@planomatic.com>
2025-02-28 11:07:18 -0700

    406 error debugging 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :log_request_headers
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
