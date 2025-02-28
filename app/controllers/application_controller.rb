7b16aa92cd9b7b9513fd48f18982ffae9f62bc12
Alex <alex@planomatic.com>
2025-02-28 11:32:54 -0700

    406 error debugging 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-  # allow_browser
+  before_action :set_default_format
+
+  private
+
+  def set_default_format
+    request.format = :json
+  end
 end
