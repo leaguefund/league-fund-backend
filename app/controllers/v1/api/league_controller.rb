4b95cd816e51fb338247e7516f334e9f529f73af
Alex <alex@planomatic.com>
2025-02-28 19:17:17 -0700

    League controller logging

--- app/controllers/v1/api/league_controller.rb
+++ app/controllers/v1/api/league_controller.rb
@@ -5,44 +5,55 @@ module V1
         before_action :identify_session
 
         def identify_session
+          logger.info("++++++1")
           # Validate Session and Username passed
           unless session_id = params[:session_id]
             return render json: { error: "no-session-id", message: "Internal Server Error (nsi)" }, status: :not_found
           end
+          logger.info("++++++2")
           # Create session variable
           $session = Session.find_or_create_by(session_id: session_id)
         end
 
         # POST /v1/api/league/created
         def created
+          logger.info("++++++3")
           # Validate League passed
           unless league_id = params[:league_id]
             return render json: { error: "no-league-id", message: "Internal Server Error (nli)" }, status: :not_found
           end
+          logger.info("++++++4")
           # Validate League Address passed
           unless league_address = params[:league_address]
             return render json: { error: "no-league-address", message: "Internal Server Error (nla)" }, status: :not_found
           end
+          logger.info("++++++5")
           # Validate Wallet Address passed
           unless wallet_address = params[:wallet_address]
             return render json: { error: "no-wallet-address", message: "Internal Server Error (nwa)" }, status: :not_found
           end
+          logger.info("++++++6")
           # Validate League Dues passed
           unless league_dues_usdc = params[:league_dues_usdc]
             return render json: { error: "no-league-dues", message: "Internal Server Error (nld)" }, status: :not_found
           end
+          logger.info("++++++6")
           # Find or create League based on sleeper_id + commissioner
           league                  = League.find_or_create_by(id: params[:league_id], commissioner_id: $session.user_id)
           league.address          = league_address
           league.dues_ucsd        = league_dues_usdc
           league.save
+          logger.info("++++++7")
           # Save user's wallet address
           $session.user.update(wallet: wallet_address)
+          logger.info("++++++8")
           # Fetch additional users
           league.sleeper_build_users
+          logger.info("++++++9")
           # Update Session Data
           $session.league_id = league.id
           $session.save
+          logger.info("++++++10")
           # Logic for league creation
           render json: { status: "success", message: "League created successfully" }, status: :ok
         ensure
